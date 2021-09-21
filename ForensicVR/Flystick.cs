e2bda35c1a22fee784e51dc86ef859edda37b9ab
George Lecakes <32460927+georgelecakes@users.noreply.github.com>
2021-09-21 09:19:09 -0400

    Mouse Controls
    Adding Mouse Controls to Simulate Flystick experience

--- ForensicVR/Flystick.cs
+++ ForensicVR/Flystick.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Flystick : MonoBehaviour
 {
+
+    Vector3 worldPoint = new Vector3();
+
     void Start()
     {
         
@@ -11,12 +14,26 @@ public class Flystick : MonoBehaviour
 
     void Update()
     {
-        
+        PointAt();
+    }
+
+    protected void PointAt()
+    {
+        Vector2 mousePos = Input.mousePosition;
+        worldPoint = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, Camera.main.nearClipPlane));
+ 
+        this.transform.LookAt(worldPoint);
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.yellow;
         Gizmos.DrawLine(this.transform.position, this.transform.position + this.transform.forward * 20.0f);
+
+        Color temp = Gizmos.color;
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawSphere(worldPoint, 0.1f);
+        Gizmos.color = temp;
+
     }
 }
